91bb61517f3b6aab1b7c79a91eba9d4f1c2ad21b
FranciscoBotta <87836164+3ottA@users.noreply.github.com>
2024-09-09 20:11:49 -0300

    logo + banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-
+import Image from "next/image";
 export default function Home() {
   return (
-    <main className="w-full min-h-screen">
+    <main className="w-full min-h-screen bg-orange-200">
+      <Image src={"/moscow-banner.jpg"} alt={""} width={1920} height={40} className="bor border-8 border-x-primary border-y-primary"></Image> 
+
     </main>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,40 @@
 import { Button, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
     return (
-        <Navbar className="bg-secondary">
+        <Navbar className="bg-primary">
+            <Image src={"/Moscow.png"} alt={""} width={40} height={40}></Image> 
+
             <NavbarBrand>
-                <p className="font-bold text-inherit">Moskow</p>
+                <p className="font-bold text-inherit text-2xl">Moskow</p>
             </NavbarBrand>
             <NavbarContent className="hidden sm:flex gap-4" justify="center">
                 <NavbarItem>
-                    <Link color="foreground" href="#">
+                    <Link color="foreground" href="/">
                         Inicio
                     </Link>
                 </NavbarItem>
                 <NavbarItem>
-                    <Link color="foreground" href="#">
+                    <Link color="foreground" href="/">
                         Categorias
                     </Link>
                 </NavbarItem>
                 <NavbarItem>
-                    <Link href="#" color="foreground">
+                    <Link href="/" color="foreground">
                         Productos
                     </Link>
                 </NavbarItem>
                 <NavbarItem>
-                    <Link color="foreground" href="#">
+                    <Link color="foreground" href="/">
                         Envios
                     </Link>
                 </NavbarItem>
             </NavbarContent>
             <NavbarContent justify="end">
                 <NavbarItem>
-                    <Button as={Link} color="primary" href="/auth/login" variant="solid">
+                    <Button as={Link} color="secondary" href="/auth/login" variant="solid">
                         Log In
                     </Button>
                 </NavbarItem>
